d339244ff3362235245997c5e1769b8f5b62cd66
Cire Markus <mrkimjarrow95@gmail.com>
2019-12-12 20:21:28 -0600

    inherited from the race class and set up initalization variables

--- Frames/Classes/Playersheet.py
+++ Frames/Classes/Playersheet.py
@@ -1,10 +1,19 @@
-from CharacterSheetClass import Charactersheet
+from raceClass import Race
 from magicalprofessions import Magicalprofession
 from nonmagical import Nonmagical
+from ProfessionClass import Profession
 
-class Player(Charactersheet):
+class Player(Race, Profession):
 	"""docstring for Player"""
-	def __init__(self, arg):
-		super(Player, self).__init__()
-		self.arg = arg
+	def __init__(self,name, level, race, profession, strength, 
+		dexterity, constitution, intelligence, 
+		wisdom, charisma):
+
+		#player core values passed to the race class
+		Race.__init__(name = name, strength = strength, dexterity = dexterity, 
+			constitution = constitution,intelligencel = intelligence, 
+			wisdom = wisdom, charisma = charisma)	
 		
+
+
+
